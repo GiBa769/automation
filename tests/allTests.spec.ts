033658a590363aea7b54ec9bca5cc90823a811c5
GiBa769 <mr.zb.gb@gmail.com>
2025-03-11 14:12:51 +0700

    Add allTests 3

--- tests/allTests.spec.ts
+++ tests/allTests.spec.ts
@@ -1,10 +1,10 @@
 import { test } from "@playwright/test";
-import "./apiTest.spec";
+//import "./apiTest.spec";
 //import "./logProcessor.spec";
-import "./loginGibasite.spec";
+//import "./loginGibasite.spec";
 //import "./ordersQuery.spec";
 //import "./sauceLogin.spec";
-import "./weatherHCM.spec";
+//import "./weatherHCM.spec";
 test.describe.parallel("Tổng hợp các tests", () => {
     test("Chạy tất cả test", async () => {
         console.log("🔹 Đã import và chạy tất cả test!");
